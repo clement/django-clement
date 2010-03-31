c31986045f12a6ee001536b743a4aac8dd69709a
Clément <clement.nodet@gmail.com>
2010-03-31 22:46:56 +0200

    urlp template tag
    similar to url, except it will try to load the request object
    from the context, and will use the overridden urlconf if
    applicable

--- django_dirox/templatetags/dirox.py
+++ django_dirox/templatetags/dirox.py
@@ -3,6 +3,7 @@ Template filters used in `moderation` application
 HTML templates. Mainly utility functions.
 """
 
+from django_dirox.template import template_function
 from django import template
 
 # Special object to allow django to recognize this
@@ -54,3 +55,35 @@ def inside(value, sequence):
         return value in sequence
     except:
         return False
+
+
+def urlp(name, *args, **kwargs):
+    """
+    Reverse a view name with corresponding arguments
+    while being aware of the request urlconf IF
+    present in the template context as "request"
+    (when using the request context processor for
+    example)
+    """
+    from django.core.urlresolvers import reverse, NoReverseMatch
+    from django.conf import settings
+
+    context = kwargs.pop('context')
+    try:
+        urlconf = context['request'].urlconf
+    except:
+        urlconf = None
+
+
+    try:
+        return reverse(name, args=args, kwargs=kwargs, urlconf=urlconf, current_app=context.current_app)
+    except NoReverseMatch, e:
+        if settings.SETTINGS_MODULE:
+            project_name = settings.SETTINGS_MODULE.split('.')[0]
+            try:
+                return reverse(project_name+'.'+name, args=args, kwargs=kwargs, urlconf=urlconf, current_app=context.current_app)
+            except NoReverseMatch:
+                raise e
+        else:
+            raise e
+urlp = register.tag(template_function(urlp, send_context=True))
