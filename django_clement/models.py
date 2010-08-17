a6a355b84f97cf8f12bca702c32f2fd538f136d1
Clément <clement.nodet@gmail.com>
2010-08-17 16:11:20 +0700

    allow changing the cache name in cache_m2m property helper
    Conflicts:
    
    	django_clement/models.py

--- django_clement/models.py
+++ django_clement/models.py
@@ -1,9 +1,11 @@
-def cache_m2m(name):
-    cname = '_'+name
+def cache_m2m(name, cache_name=None):
+    if not cache_name:
+        cache_name = '_'+name
+
     def fget(self):
-        if not hasattr(self, cname):
-            setattr(self, cname, getattr(self, name).all())
-        return getattr(self, cname)
+        if not hasattr(self, cache_name):
+            setattr(self, cache_name, getattr(self, name).all())
+        return getattr(self, cache_name)
     def fset(self, value):
-        setattr(self, cname, value)
+        setattr(self, cache_name, value)
     return property(fget, fset)
